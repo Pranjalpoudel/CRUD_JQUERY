4ac85850b96fc3dbd2eac819f70ffa72b50e8c6c
Pranjal Poudel <pranjalpaudel62@gmail.com>
2026-02-14 07:57:29 +0545

    new exercise dashboard

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* Gym Workout Tracker - jQuery Application */
 $(function() {
     'use strict';
-    // App logic will be added in subsequent commits
+    $('#exercise-date').val(new Date().toISOString().slice(0, 10));
 });
